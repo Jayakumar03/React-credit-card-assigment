b1cb6cf7f3a1485c9a4013893c30a28e45406df0
jayakumar03 <jaimech61@gmail.com>
2023-09-14 09:42:48 +0530

    Changed width of section and added class for button

--- src/cardDetails.jsx
+++ src/cardDetails.jsx
@@ -1,44 +1,50 @@
 export const CardDetails = () => {
   return (
     // border-dashed border-2 border-sky-500
-    <div className=" w-1/3 h-2/4 relative left-[60%] bottom-[90%]">
+    <div className=" w-1/3 h-2/4 relative left-[50%] bottom-[90%]">
       <form className=" font-mono flex flex-col gap-5">
+        {/* Card holder name section */}
         <div className="py-1">
           <p>CARDHOLDER NAME</p>
           <input
-            className=" border-[1px] border-slate-500 rounded-md w-[100%] py-1 my-1 focus:outline-none focus:border-red-500 focus:ring-3 focus:ring-sky-500   "
+            className=" border-[1px] border-slate-500 rounded-md w-3/4 py-1 my-1 focus:outline-none focus:border-purple-500    "
             type="text"
             placeholder="Name In Card"
-            
           ></input>
         </div>
+
+        {/* Card name section */}
         <div className="py-1  ">
           <p>CARD NUMBER</p>
           <input
-            className=" border-[1px] border-slate-500 rounded-md w-[100%] py-1 focus:outline-none focus:border-red-500 focus:ring-3 focus:ring-sky-500  "
+            className=" border-[1px] border-slate-500 rounded-md w-3/4 py-1 focus:outline-none focus:border-purple-500  "
             type="NUMBER"
             placeholder="CARD NUMBER"
           ></input>
         </div>
-        <div className="flex py-1 gap-3 justify-between focus:outline-none focus:border-red-500 focus:ring-3 focus:ring-sky-500  ">
-          <p className="w-1/4">EXP.DATE</p>
+
+        {/* Card details section */}
+        <div className="flex  py-1 focus:outline-none focus:border-purple-500 ">
+          <p >EXP.DATE</p>
           <p className="w-1/4">(MM.YY)</p>
-          <p className="w-1/4">CVC</p>
+          <p className="w-1/4 relative left-[7%]">CVC</p>
         </div>
 
-        <div className="flex gap-3 justify-between">
+        {/* Card details input section */}
+
+        <div className="flex gap-3 ">
           <input
-            className="w-1/4 border-[1px] border-slate-500 rounded-md focus:outline-none focus:border-red-500 focus:ring-3 focus:ring-sky-500 "
+            className="w-[10%] border-[1px] border-slate-500 rounded-md focus:outline-none focus:border-purple-500"
             type="NUMBER"
             placeholder="MM"
           ></input>
           <input
-            className="w-1/4 border-[1px] border-slate-500 rounded-md focus:outline-none focus:ring focus:ring-red-300"
+            className="w-[10%] border-[1px] border-slate-500 rounded-md focus:outline-none focus:border-purple-500"
             type="NUMBER"
             placeholder="YY"
           ></input>
           <input
-            className="w-1/4 border-[1px] border-slate-500 rounded-md focus:outline-none focus:border-red-500 focus:ring-3 focus:ring-sky-500 "
+            className="relative left-[20%] w-[30%] border-[1px] border-slate-500 rounded-md focus:outline-none focus:border-purple-500 "
             type="NUMBER"
             placeholder="e.g. 123"
           ></input>
@@ -46,7 +52,7 @@ export const CardDetails = () => {
       </form>
 
       <button
-        className="w-[100%] h-8 border-[1px] bg-purple-700 border-slate-500 rounded-md focus:outline-none hover:bg-white-600 focus:border-red-500 focus:ring-3 focus:ring-sky-500 relative top-8 text-center"
+        className="w-3/4 h-8 border-[1px] bg-purple-700 border-slate-500 rounded-md relative top-8 text-center  hover:bg-violet-800 "
         type="submit"
       >
         Confirm
